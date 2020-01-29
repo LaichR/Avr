ea4dc1f96c7ec63f5c1229bb621059a1bce03032
LaichR <46196385+LaichR@users.noreply.github.com>
2020-01-29 07:11:05 +0100

    cleanup files

--- HelloWorld/HelloWorld/main.c
+++ HelloWorld/HelloWorld/main.c
@@ -30,7 +30,61 @@ char helloWorld[] = "hello world\n";
 
 int main(void)
 {
-
+	InitializeServoCtrl();
+	DDRB = 0xFF; // set port b as output!
+	DDRA = 0;	 // set port a as input!
+	
+	volatile uint8_t portB = 0xFF;
+	uint16_t i = 0;
+	uint16_t j = 0;    
+	Usart_Init();
+	uint8_t userInput = 1;
+	
+	char *chPtr = helloWorld;
+	while(*chPtr)
+	{
+		Usart_PutChar(*chPtr++);
+	}
+	
+    while (userInput) 
+    {	
+		
+		
+		//CatchRace();
+		
+		PORTB = portB;
+		portB = ~portB;
+		
+		//for( i = 0; i < 10; i++)
+		{
+			for(j = 0; j < 100; j++);
+		}
+		
+		
+		userInput = PINA&1;
+		//Usart_PutChar(userInput);
+		static AvrMessage msg;
+		
+		if ( Usart_GetMessage(&msg) )
+		{
+			uint8_t i = 0;
+			if( msg.MsgType == PacketType_TestCommand)
+			{
+				if( msg.Payload[0] == CmdIdServoPos )
+				{
+					Usart_PutChar(0xAA);
+					SetPosition(msg.Payload[1]);
+				}
+			}
+			while( i < msg.Length)
+			{
+				Usart_PutChar(msg.Payload[i++]);
+			}	
+		}
+		
+		
+		
+    }
 }
 
 
@@ -45,7 +99,7 @@ void CatchRace(void)
 		Usart_PutChar(0xAA);
 		Usart_PutChar(oldTimer&0xFF);
 		Usart_PutChar(oldTimer>>8);
-		Usart_PutChar(0xAA);
+		Usart_PutChar(0xAB);
 		Usart_PutChar(newTimer&0xFF);
 		Usart_PutChar(newTimer>>8);
 	}
