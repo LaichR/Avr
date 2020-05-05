e1eacc0e5963ab068d288025a029ea7d0b689b8c
LaichR <46196385+LaichR@users.noreply.github.com>
2020-05-05 13:56:21 +0200

    another fix to setupenvironment.bat

--- SetupEnvironment.bat
+++ SetupEnvironment.bat
@@ -13,9 +13,9 @@ set AvrGcc=%ToolsRoot%\avr8-gnu-toolchain-win32_x86
 set AvrDude=%ToolsRoot%\avrdude-6.3-mingw32
 set DotNetLib=%ToolsRoot%\DotNetLib
 set Batch=%ToolsRoot%\Batch
-if not exist %ProjectRoot% mkdir %ProjectRoot%
-if not exist %ProjectRoot%\Build mkdir %ProjectRoot%\Build
-if not exist %ProjectRoot%\make.bat copy %Batch%\make.bat %ProjectRoot%
-if not exist %ProjectRoot%\%Project%.vcxproj python.exe %PyScripts%\GenerateVsProject.py
-cd %ProjectRoot%
+if not exist "%ProjectRoot%\" mkdir "%ProjectRoot%"
+if not exist "%ProjectRoot%\Build\" mkdir "%ProjectRoot%\Build"
+if not exist "%ProjectRoot%\make.bat" copy "%Batch%\make.bat" "%ProjectRoot%"
+if not exist "%ProjectRoot%\%Project%.vcxproj" python.exe "%PyScripts%\GenerateVsProject.py"
+cd "%ProjectRoot%"
 call cmd.exe /K echo  [32m **  environment set to %Project% ** [0m
